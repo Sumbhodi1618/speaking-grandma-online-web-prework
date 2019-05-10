a967b9567a8172bbd15ec3d35c3ad5396cbd502e
Justin <sumbhodi1618@gmail.com>
2019-05-10 22:50:03 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,10 +1,7 @@
-def speak_to_grandma
-  speak = gets
-  if speak == "Hi Nana, how are you?"
-    return "HUH?! SPEAK UP, SONNY!"
-  elsif speak == "WHAT DID YOU EAT TODAY?"
-    return "NO, NOT SINCE 1938!"
+def speak_to_grandma(speak)
+  if speak == speak.downcase
+   return "HUH?! SPEAK UP, SONNY!"
   else speak == "I LOVE YOU GRANDMA!"
-    return "I LOVE YOU TOO PUMPKIN!".upcase
+    return "I LOVE YOU TOO PUMPKIN!"
   end
- end
+end
